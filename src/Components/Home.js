5530f4b063efbcd0b575d2d61ac024aae4228b1f
brandondoris <brandonmdoris@yahoo.com>
2021-10-21 16:49:35 -0400

    Created home component

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+const Home =(props) => {
+    return (
+        <div>
+            <h1>Welcome to TV Tracker</h1>
+        </div>
+    );
+}
+
+export default Home;
